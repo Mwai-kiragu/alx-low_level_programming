b492b25dd3afdd08944219e23a524667e1df3b61
Mwai-Kiragu <onesmusmwai40@gmail.com>
2022-09-08 05:38:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,11 @@
 #include <unistd.h>
 
 /**
- *main - Entry point
- *Return: Always 1
- * 
+ * main - print the string in write function
+ *
+ * Description: using the main function 
+ * the program prints and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Return: Always 1(Success)
  */
 
 int main(void)
