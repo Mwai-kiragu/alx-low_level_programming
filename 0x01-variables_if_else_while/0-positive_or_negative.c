66457e51b294ac2895030901d3d85434d1d0f71b
Mwai-Kiragu <onesmusmwai40@gmail.com>
2022-09-09 11:31:18 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,9 @@
 #include <stdio.h>
 
 /**
- *  main - print the string in the put function
- * 
- *  Description: using the main function
- *  the program prints "Programming is like building a multilingual puzzle"
- *  Return: Always 0 (Success)
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
