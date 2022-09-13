43873087b5fe22b8b70a03e564130d52e521a4b0
Mwai-Kiragu <onesmusmwai40@gmail.com>
2022-09-13 08:03:36 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *m = "_putchar";
+
+	while (*m)
+	{
+		_putchar(*m);
+		m++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
